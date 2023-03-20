a8d3c1761ac121decee5317eb4ba78980b406b5f
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2023-03-19 20:13:36 -0600

    router

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -3,18 +3,16 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Logo from "./images/LogoAmazon.png"
-import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
 
   return (
-    <div className="container mx-auto">
-      <nav className="bg-gray-100 flex grap-4">
-        <Link to="/Home"><span>Home</span></Link>
-        <Link to="/Contact"><span>Contact</span></Link>
-        <Link to="/About"><span>About</span></Link>
-      </nav>
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+    </Routes>
   )
 }
 
-  export default App
+  export default App;

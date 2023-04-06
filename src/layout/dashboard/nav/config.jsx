d241d6d0fdab2047c7917d8fe40be911104bce9c
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2023-04-06 11:36:46 -0600

    avance

--- src/layout/dashboard/nav/config.jsx
+++ src/layout/dashboard/nav/config.jsx
@@ -1,40 +1,34 @@
 // component
 import SvgColor from '../../../components/svg-color';
-
+import DeleteIcon from '@mui/icons-material/Delete';
 // ----------------------------------------------------------------------
 
-const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
 
 const navConfig = [
   {
     title: 'dashboard',
     path: '/dashboard/app',
-    icon: icon('ic_analytics'),
   },
   {
     title: 'user',
     path: '/dashboard/user',
-    icon: icon('ic_user'),
   },
   {
     title: 'product',
     path: '/dashboard/products',
-    icon: icon('ic_cart'),
+    icon: <DeleteIcon />,
   },
   {
     title: 'blog',
     path: '/dashboard/blog',
-    icon: icon('ic_blog'),
   },
   {
     title: 'login',
     path: '/login',
-    icon: icon('ic_lock'),
   },
   {
     title: 'Not found',
     path: '/404',
-    icon: icon('ic_disabled'),
   },
 ];
 
